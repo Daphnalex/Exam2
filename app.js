ebace9cd2b1bbbb758363e1d89e26a021e2df38a
Daphnalex <dady.luthien@hotmail.fr>
2016-11-09 15:39:29 +0100

    examen 2

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 (function(){
+ var app = angular.module('app', []);
 
+	app.directive('header', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'partials/common/header.html'
+		}
+	});
+	app.directive('footer', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'partials/common/footer.html'
+		}
+	});
+	app.directive('home', function(){
+		return {
+			restrict: 'A',
+			templateUrl: 'partials/home/home.html'
+		}
+	});
 
-
-
-
+	app.controller('appController', function(){
+		this.personne = object;
+	});
 
   var object = {
     name : "Tian Bao",
